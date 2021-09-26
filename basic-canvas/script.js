94346c16c3df8a10c0fbf47cddc7e0a4eafb9f0a
Chung Byung Jae <jbj828@naver.com>
2021-09-26 21:56:45 +0900

    Study canvas api

---
+++ basic-canvas/script.js
@@ -0,0 +1,171 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// // fillRect()
+// ctx.fillStyle = 'red';
+// ctx.fillRect(20, 20, 150, 100);
+// ctx.fillStyle = 'blue';
+// ctx.fillRect(50, 20, 150, 100);
+
+// // strokeRect()
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = 'green';
+// ctx.strokeRect(100, 200, 150, 100);
+
+// // clearRect()
+// ctx.clearRect(25, 25, 140, 90);
+
+// // fillText
+// ctx.font = '30px Arial';
+// ctx.fillStyle = 'purple';
+// ctx.fillText('Hello world', 400, 50);
+
+// // strokeText()
+// ctx.lineWidth = 1;
+// ctx.strokeStyle = 'orange';
+// ctx.strokeText('Hello World', 400, 100);
+
+// // Paths
+
+// // Triangle
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(100, 200);
+// // ctx.lineTo(50, 50);
+// ctx.closePath();
+// // ctx.stroke();
+// ctx.fillStyle = 'coral';
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.moveTo(200, 50);
+// ctx.lineTo(150, 200);
+// ctx.lineTo(250, 200);
+// ctx.closePath();
+// ctx.fillStyle = 'blue';
+// ctx.fill();
+
+// // Rectangle
+// ctx.beginPath();
+// ctx.rect(300, 50, 150, 100);
+// ctx.fillStyle = 'teal';
+// ctx.fill();
+
+// // Arc (circles)
+// ctx.beginPath();
+
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
+
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+
+// // Move to mouth
+// ctx.moveTo(centerX + 100, centerY);
+
+// // Draw mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+
+// // Move to left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
+
+// // Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+
+// // Move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
+
+// // Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+
+// ctx.stroke();
+
+// // Animation 1
+
+// const circle = {
+//   x: 200,
+//   y: 200,
+//   size: 30,
+//   dx: 5,
+//   dy: 4,
+// };
+
+// function drawCircle() {
+//   ctx.beginPath();
+//   ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//   ctx.fillStyle = 'purple';
+//   ctx.fill();
+// }
+
+// function update() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//   drawCircle();
+
+//   // change position
+//   circle.x += circle.dx;
+//   circle.y += circle.dy;
+
+//   // Detect side walls
+//   if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+//     circle.dx *= -1;
+//   }
+
+//   // Detect top and bottom walls
+//   if (circle.y + circle.size > canvas.width || circle.y - circle.size < 0) {
+//     circle.dy *= -1;
+//   }
+
+//   requestAnimationFrame(update);
+// }
+
+// update();
+
+// Animation 2 - Character
+const image = document.getElementById('source');
+const player = {
+  w: 50,
+  h: 70,
+  x: 20,
+  y: 200,
+  speed: 5,
+  dx: 0,
+  dy: 0,
+};  
+
+function drawPlayer() {
+  ctx.drawImage(image, player.x, player.y, player.w, player.h);
+}
+
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function newPos() {
+  player.x += player.dx;
+  player.y += player.dy;
+}
+
+function update() {
+  clear();
+
+  drawPlayer();
+  newPos();
+
+  requestAnimationFrame(update);
+}
+
+function keyDown(e) {
+  if(e.key === 'ArrowRight' || e.key === 'Right') {
+    moveRight()
+  } else if(e.key )
+}
+function keyUp(e) {
+
+}
+
+update();
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
